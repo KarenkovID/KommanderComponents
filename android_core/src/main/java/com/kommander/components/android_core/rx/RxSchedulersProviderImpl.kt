93e1c0365fbc40bdb2a7bb1c7c90266a9016d3a6
ikarenkov <ikarenkov@okko.tv>
2019-12-26 13:50:44 +0300

    Use kotlin InjectConstructor annotation

--- android_core/src/main/java/com/kommander/components/android_core/rx/RxSchedulersProviderImpl.kt
+++ android_core/src/main/java/com/kommander/components/android_core/rx/RxSchedulersProviderImpl.kt
@@ -3,9 +3,11 @@ package com.kommander.components.android_core.rx
 import com.kommander.components.domain_core.rx.RxSchedulersProvider
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import toothpick.InjectConstructor
 import javax.inject.Inject
 
-class RxSchedulersProviderImpl @Inject constructor() : RxSchedulersProvider {
+@InjectConstructor
+class RxSchedulersProviderImpl : RxSchedulersProvider {
 
     override fun io() = Schedulers.io()
 
