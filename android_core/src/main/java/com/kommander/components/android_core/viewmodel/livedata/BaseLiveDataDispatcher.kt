2e8b6e7740cd3b46c2d88e9711b0883630c57f6a
ikarenkov <ikarenkov@okko.tv>
2020-01-15 18:53:04 +0300

    Fix package

--- android_core/src/main/java/com/kommander/components/android_core/viewmodel/livedata/BaseLiveDataDispatcher.kt
+++ android_core/src/main/java/com/kommander/components/android_core/viewmodel/livedata/BaseLiveDataDispatcher.kt
@@ -1,14 +1,12 @@
-package com.itmoclimbing.internal.viewmodel.livedata
+package com.kommander.components.android_core.viewmodel.livedata
 
 import androidx.lifecycle.MutableLiveData
-import com.kommander.components.android_core.viewmodel.livedata.ContentEvent
 import io.reactivex.Completable
 import io.reactivex.Flowable
 import io.reactivex.Maybe
 import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.disposables.Disposable
-import com.kommander.components.android_core.viewmodel.livedata.Event
 
 fun <T> Flowable<out T>.dispatchTo(liveData: MutableLiveData<ContentEvent<T>>): Disposable {
     liveData.value = ContentEvent.Loading(liveData.value?.data)
